436d0a67f41be010f1780d623dc64b75854b935d
yuko-kochi <happy.k165131@gmail.com>
2021-06-16 10:35:00 +0000

    [Add]_new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,9 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -13,22 +14,23 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @books = Book.find(params[:id])
   end
-  
+
   def edit
    @book = Book.find(params[:id])
   end
 
   def destroy
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
