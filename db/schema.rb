98a2f0578d261e9807295f3ee9eb138f606c499c
yuko-kochi <happy.k165131@gmail.com>
2021-07-31 09:25:25 +0000

    credentials.yml.enc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_113733) do
+ActiveRecord::Schema.define(version: 2021_06_15_074340) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
